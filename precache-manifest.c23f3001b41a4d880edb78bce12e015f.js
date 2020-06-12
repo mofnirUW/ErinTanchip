a62a6d03fc4bfdb89a695797a70f2a83a1c6e3c0
Corbin Nicholas <nlatanchip@gmail.com>
2020-06-12 17:04:06 -0400

    Updates

---
+++ precache-manifest.c23f3001b41a4d880edb78bce12e015f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b35e77227f6f277d47e93b9d4a666b",
+    "url": "/ErinTanchip/index.html"
+  },
+  {
+    "revision": "986429f73db9b73fd1d2",
+    "url": "/ErinTanchip/static/js/2.f5e0e93b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ErinTanchip/static/js/2.f5e0e93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3441218ae36acaea0db",
+    "url": "/ErinTanchip/static/js/main.42a84b89.chunk.js"
+  },
+  {
+    "revision": "cea671b79df8dbfe3e49",
+    "url": "/ErinTanchip/static/js/runtime-main.03860d28.js"
+  }
+]);
